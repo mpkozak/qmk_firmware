41ed84bc6006070d8927e8ebc06a64248a5eaad7
mpkozak <mparkerkozak@gmail.com>
2023-04-27 21:50:35 -0400

    restored num row to spd layers in q2 + q60.

--- keyboards/keychron/q60/ansi/keymaps/kozak/keymap.c
+++ keyboards/keychron/q60/ansi/keymaps/kozak/keymap.c
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                     KC_LOPTN, KC_LCMMD,                               LT(2,KC_SPC),                           KC_RCMMD, KC_ROPTN),
 
     [BASE_SPD] = LAYOUT_ansi_60(    // layer 1
-        TO(0),     KC_EXLM, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_LPRN, KC_RPRN, KC_PMNS, KC_NO,   KC_NO,   KC_BSPC,
+        TO(0),     KC_1,    KC_P2,   KC_P3,   KC_P4,   KC_P5,   KC_P6,   KC_P7,   KC_P8,   KC_9,    KC_0,    KC_PMNS, KC_NO,   KC_NO,   KC_BSPC,
         KC_TAB,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_NO,   KC_NO,          A(KC_BSPC),
         KC_NO,         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,               KC_ENT,
         OSM(MOD_LSFT),   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_PDOT, KC_QUES,             KC_RSFT, MO(2),
