5984d11d7cdc1fe73bf91f6a77eed19aa4d52fed
mpkozak <mparkerkozak@gmail.com>
2023-05-07 12:15:56 -0400

    updates to q2 keymap.

--- keyboards/keychron/q2/ansi_encoder/keymaps/kozak/keymap.c
+++ keyboards/keychron/q2/ansi_encoder/keymaps/kozak/keymap.c
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      // _______,   _______,   _______,                          _______,                          _______,  _______,  _______,   _______, _______, _______), // r4
 
     [BASE] = LAYOUT_ansi_67(        // layer 0 - white
-        LT(3,KC_ESC), KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,    KC_BSPC,       KC_MUTE,
+        KC_ESC,       KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,    KC_BSPC,       KC_MUTE,
         KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,       LT(4,KC_GRV),
         KC_CAPS,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,        KC_ENT,        LT(3,KC_BSLS),
         KC_LSFT,            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,      KC_RSFT,       KC_UP,
@@ -78,8 +78,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE_FN] = LAYOUT_ansi_67(        // layer 2 - cyan
         KC_LSCR,      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,    _______,       KC_ZACT,
         KC_TAB,         KC_MVLT, KC_MVRT, KC_EMOC, KC_NO,   TG(1),   KC_NO,   KC_MVLS, KC_UP,   KC_MVRS, KC_NO,   KC_BRID, KC_BRIU, KC_BSPC,       _______,
-        KC_CAPS,          KC_MVLS, KC_MVRS, KC_F11,  KC_FST,  KC_NO,   KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_MVLS, KC_MVRS,        TT(4),         _______,
-        KC_LSFT,            KC_ZMNS, KC_ZPLS, KC_ZACT, KC_PSTT, KC_WBAK, KC_WFOR, QK_KB_0, KC_VOLD, KC_VOLU, KC_MUTE,      TT(3),         QK_KB_0,
+        KC_CAPS,          KC_MVLS, KC_MVRS, KC_F11,  KC_FST,  KC_NO,   KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_MVLS, KC_MVRS,        KC_ENT,        _______,
+        KC_LSFT,            KC_ZMNS, KC_ZPLS, KC_ZACT, KC_PSTT, KC_WBAK, KC_WFOR, QK_KB_0, KC_VOLD, KC_VOLU, KC_MUTE,      KC_RSFT,       QK_KB_0,
         KC_LCTL,   KC_LOPTN,  KC_LCMMD,                         _______,                          TT(4),    TT(3),    _______,   KC_MVLS, KC_F11,  KC_MVRS),
 
     [_FN1] = LAYOUT_ansi_67(        // layer 3 - green
