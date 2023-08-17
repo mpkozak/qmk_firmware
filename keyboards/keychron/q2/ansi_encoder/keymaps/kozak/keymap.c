588dadc04518cf6bda527d15cce398d4e20a085b
mpkozak <mparkerkozak@gmail.com>
2023-08-17 13:41:32 -0400

    updated layer0 caps to ctrl on q2.

--- keyboards/keychron/q2/ansi_encoder/keymaps/kozak/keymap.c
+++ keyboards/keychron/q2/ansi_encoder/keymaps/kozak/keymap.c
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_ansi_67(        // layer 0 - white
         KC_ESC,       KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,    KC_BSPC,       KC_MUTE,
         KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,       LT(4,KC_GRV),
-        KC_CAPS,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,        KC_ENT,        LT(3,KC_BSLS),
+        KC_LCTL,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,        KC_ENT,        LT(3,KC_BSLS),
         KC_LSFT,            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,      KC_RSFT,       KC_UP,
         KC_LCTL,   KC_LOPT,   OSM(MOD_LGUI),                    LT(2,KC_SPC),                     KC_RCMD,  KC_ROPT,  TT(3),     KC_LEFT, KC_DOWN, KC_RGHT),
 
