e4680199cb50bdc4097371b09c992befe6cdb8fc
mpkozak <mparkerkozak@gmail.com>
2023-09-27 19:32:19 -0400

    changed TT() to MO() for layer 3 on q2+q60.

--- keyboards/keychron/q2/ansi_encoder/keymaps/kozak/keymap.c
+++ keyboards/keychron/q2/ansi_encoder/keymaps/kozak/keymap.c
@@ -84,14 +84,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,       LT(4,KC_GRV),
         KC_LCTL,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,        KC_ENT,        LT(3,KC_BSLS),
         KC_LSFT,            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,      KC_RSFT,       KC_UP,
-        KC_LCTL,   KC_LOPT,   OSM(MOD_LGUI),                    LT(2,KC_SPC),                     KC_RCMD,  KC_ROPT,  TT(3),     KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LCTL,   KC_LOPT,   OSM(MOD_LGUI),                    LT(2,KC_SPC),                     KC_RCMD,  KC_ROPT,  MO(3),     KC_LEFT, KC_DOWN, KC_RGHT),
 
     [BASE_SPD] = LAYOUT_ansi_67(    // layer 1 - cyan
         TO(0),        KC_1,    KC_P2,   KC_P3,   KC_P4,   KC_P5,   KC_P6,   KC_P7,   KC_P8,   KC_9,    KC_0,    KC_MINS, KC_NO,     KC_BSPW,       KC_MUTE,
         KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_NO,   KC_NO,   KC_BSPC,       LT(4,KC_GRV),
         KC_NO,            KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,        KC_ENT,        LT(3,KC_BSLS),
         OSM(MOD_LSFT),      KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_QUES,      KC_RSFT,       KC_UP,
-        KC_LCTL,   KC_LOPT,   KC_LCMD,                          LT(2,KC_SPC),                     KC_NO,    KC_NO,    TT(3),     KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LCTL,   KC_LOPT,   KC_LCMD,                          LT(2,KC_SPC),                     KC_NO,    KC_NO,    MO(3),     KC_LEFT, KC_DOWN, KC_RGHT),
 
     [BASE_FN] = LAYOUT_ansi_67(        // layer 2 - blue
         TO(0),        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,    KC_BSPC,       KC_ZACT,
