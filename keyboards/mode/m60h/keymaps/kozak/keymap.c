b62d31849b549478e6f164030efda0cea82b17b8
mpkozak <mparkerkozak@gmail.com>
2024-01-15 02:41:52 -0500

    cleanup tempo.

--- keyboards/mode/m60h/keymaps/kozak/keymap.c
+++ keyboards/mode/m60h/keymaps/kozak/keymap.c
@@ -18,9 +18,6 @@
 // #include "keymap_user_config.h"
 #include "keymap_user.h"
 
-// #define KC_MCTL QK_KB_0
-// #define KC_LPAD QK_KB_1
-
 #define KC_LSCR C(G(KC_Q))      // lock screen
 #define KC_EMOC C(G(KC_SPC))    // character picker
 #define KC_FSTG C(G(KC_F))      // fullscreen toggle
@@ -88,23 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           _______, _______,                   _______,                            _______, _______
     )
 
-    // [BASE_SPD] = LAYOUT_60_hhkb(   // layer 1 - cyan
-    //     TO(0),   KC_1,    KC_P2,   KC_P3,   KC_P4,   KC_P5,   KC_P6,   KC_P7,   KC_P8,   KC_9,    KC_0,    KC_MINS, KC_NO,   KC_NO,   KC_BSPW,
-    //     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_NO,   KC_NO,   KC_BSPC,
-    //     KC_NO,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
-    //     OSM(MOD_LSFT),    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(3),
-    //                       KC_LCTL, KC_LCMD,                   LT(2,KC_SPC),                       KC_RCMD, KC_ROPT
-    // ),
-
 };
-
-
-
-// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-//     return process_record_kozak(keycode, record);
-// }
-
-
 
 const rgblight_segment_t PROGMEM rbg_layer_base[] = RGBLIGHT_LAYER_SEGMENTS(
     {0, 20, BASE_LAYER_HSV}
@@ -146,6 +127,14 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     rgblight_set_layer_state(3, layer_state_cmp(state, _FN2));
     return state;
 }
+
+
+
+
+
+// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+//     return process_record_kozak(keycode, record);
+// }
 
 
 
---
+++ keyboards/mode/m60h/keymaps/kozak/keymap_user_config.c
@@ -0,0 +1,51 @@
+/* Copyright 2023 @ M. Parker Kozak (https://github.com/mpkozak)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// #include QMK_KEYBOARD_H
+// #include "keymap_user_config.h"
+
+// static uint8_t mac_keycode[4] = { KC_LOPT, KC_ROPT, KC_LCMD, KC_RCMD };
+
+// bool process_record_kozak(uint16_t keycode, keyrecord_t *record) {
+//     switch (keycode) {
+//         case QK_KB_0:
+//             if (record->event.pressed) {
+//                 register_code(KC_MISSION_CONTROL);
+//             } else {
+//                 unregister_code(KC_MISSION_CONTROL);
+//             }
+//             return false;  // Skip all further processing of this key
+//         case QK_KB_1:
+//             if (record->event.pressed) {
+//                 register_code(KC_LAUNCHPAD);
+//             } else {
+//                 unregister_code(KC_LAUNCHPAD);
+//             }
+//             return false;  // Skip all further processing of this key
+//         case KC_LOPTN:
+//         case KC_ROPTN:
+//         case KC_LCMMD:
+//         case KC_RCMMD:
+//             if (record->event.pressed) {
+//                 register_code(mac_keycode[keycode - KC_LOPTN]);
+//             } else {
+//                 unregister_code(mac_keycode[keycode - KC_LOPTN]);
+//             }
+//             return false;  // Skip all further processing of this key
+//         default:
+//             return true;  // Process all other keycodes normally
+//     }
+// }
