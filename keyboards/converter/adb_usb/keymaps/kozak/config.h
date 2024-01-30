953127439d3410277a801248728e81ef1aa62d03
mpkozak <mparkerkozak@gmail.com>
2024-01-30 01:55:18 -0500

    added layer + mod taps to adb m0116.

---
+++ keyboards/converter/adb_usb/keymaps/kozak/config.h
@@ -0,0 +1,13 @@
+
+
+// Override Layer count
+#undef DYNAMIC_KEYMAP_LAYER_COUNT
+#define DYNAMIC_KEYMAP_LAYER_COUNT 2
+
+// Faster tap threshold
+#define TAPPING_TERM 160
+#undef PERMISSIVE_HOLD
+#undef PREVENT_STUCK_MODIFIERS
+
+// One-shot timeout
+#define ONESHOT_TIMEOUT 500
