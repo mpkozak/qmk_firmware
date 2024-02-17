f53e5b33819c85b4149f748aa93f92a4c846670b
mpkozak <mparkerkozak@gmail.com>
2024-02-16 23:33:40 -0500

    added support for m0110 WIP.

--- _kozak/init.sh
+++ _kozak/init.sh
@@ -21,6 +21,7 @@ _q2="$QMK_HOME/keyboards/keychron/q2"
 _q60="$QMK_HOME/keyboards/keychron/q60"
 _tempo="$QMK_HOME/keyboards/mode/m60h"
 _adb="$QMK_HOME/keyboards/converter/adb_usb"
+_m0110="$QMK_HOME/keyboards/converter/m0110_usb"
 
 # custom keymaps
 _q0km="$_q0/plus/keymaps/kozak"
@@ -28,6 +29,7 @@ _q2km="$_q2/ansi_encoder/keymaps/kozak"
 _q60km="$_q60/ansi/keymaps/kozak"
 _tempokm="$_tempo/keymaps/kozak"
 _adbkm="$_adb/keymaps/kozak"
+_m0110km="$_m0110/keymaps/kozak"
 
 # customized keyboards
 _q0c="keychron/q0/plus"
@@ -35,6 +37,7 @@ _q2c="keychron/q2/ansi_encoder"
 _q60c="keychron/q60/ansi"
 _tempoc="mode/m60h"
 _adbc="converter/adb_usb/rev1"
+_m0110c="converter/m0110_usb"
 
 # build filenames
 _q0f="keychron_q0_plus_kozak.bin"
@@ -42,6 +45,7 @@ _q2f="keychron_q2_ansi_encoder_kozak.bin"
 _q60f="keychron_q60_ansi_kozak.bin"
 _tempof="mode_m60h_kozak.bin"
 _adbf="converter_adb_usb_rev1_kozak.hex"
+_m0110f="converter_m0110_usb_kozak.hex"
 
 
 # fs aliases
@@ -54,12 +58,14 @@ alias q2='cd "$_q2"; e'
 alias q60='cd "$_q60"; e'
 alias tempo='cd "$_tempo"; e'
 alias adb='cd "$_adb"; e'
+alias m0110='cd "$_m0110"; e'
 alias ek='e "$KOZAK"'
 alias e0='cd $_q0km; e'
 alias e2='cd $_q2km; e'
 alias e60='cd $_q60km; e'
 alias etempo='cd $_tempokm; e'
 alias eadb='cd $_adbkm; e'
+alias em0110='cd "$_m0110km"; e'
 
 # autocorrect aliases
 alias ace='e "$KOZAK"/autocorrect_dictionary.txt'
@@ -72,7 +78,6 @@ alias acrm="acrm_q2 && acrm_q60 && acrm_tempo"
 alias acg_q2='qmk generate-autocorrect-data "$KOZAK"/autocorrect_dictionary.txt -kb keychron/q2/ansi_encoder -km kozak'
 alias acg_q60='qmk generate-autocorrect-data "$KOZAK"/autocorrect_dictionary.txt -kb keychron/q60/ansi -km kozak'
 alias acg_tempo='qmk generate-autocorrect-data "$KOZAK"/autocorrect_dictionary.txt -kb mode/m60h -km kozak'
-# alias acg="acg_q2 && acg_q60 && acg_tempo"
 alias acg="acg_q2 && acg_q60"
 
 alias acr="acrm; acg"
@@ -113,6 +118,11 @@ function build {
         board_name="m0116"
         kb="$_adbc"
         file="$_adbf"
+        ext="hex"
+    elif [[ "$1" == "m0110" ]]; then
+        board_name="m0110"
+        kb="$_m0110c"
+        file="$_m0110f"
         ext="hex"
     else
         echo "invalid selection: $1"
