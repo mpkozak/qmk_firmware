82888999902a9390be4607200419f26a00df19f8
mpkozak <mparkerkozak@gmail.com>
2024-03-23 20:35:54 -0400

    attempting new build + layout for m0110.

--- keyboards/converter/m0110_usb/keymaps/kozak-m0110/rules.mk
+++ keyboards/converter/m0110_usb/keymaps/kozak-m0110/rules.mk
@@ -6,8 +6,8 @@ F_CPU = 16000000
 #
 BOOTMAGIC_ENABLE = yes    # Enable Bootmagic Lite
 MOUSEKEY_ENABLE  = no     # Mouse keys
-CONSOLE_ENABLE   = no     # Console for debug
-COMMAND_ENABLE   = no     # Commands for debug and configuration
+CONSOLE_ENABLE   = yes     # Console for debug
+COMMAND_ENABLE   = yes     # Commands for debug and configuration
 NKRO_ENABLE      = no     # Enable N-Key Rollover
 EXTRAKEY_ENABLE  = yes
 USB_HID_ENABLE   = yes
