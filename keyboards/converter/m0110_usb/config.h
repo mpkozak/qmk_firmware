c2dea987e0560a556ca09c7163623e74cd8e1fd6
mpkozak <mparkerkozak@gmail.com>
2024-04-18 13:07:15 -0400

    trying to fix keymap for m0110.

--- keyboards/converter/m0110_usb/config.h
+++ keyboards/converter/m0110_usb/config.h
@@ -26,9 +26,9 @@ Ported to QMK by Techsock <info@techsock.com>
 #define MATRIX_COLS 8
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
+// #define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
+// #define LOCKING_RESYNC_ENABLE
 
 /* magic key */
 #define IS_COMMAND() ( \
