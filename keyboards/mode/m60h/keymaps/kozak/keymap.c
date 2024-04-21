28a20030d9e7239373f1945f07dd46499d404e02
mpkozak <mparkerkozak@gmail.com>
2024-04-21 14:45:05 -0400

    updated arrows layout on tempo.

--- keyboards/mode/m60h/keymaps/kozak/keymap.c
+++ keyboards/mode/m60h/keymaps/kozak/keymap.c
@@ -39,10 +39,10 @@
 #define KC_REFR LSG(KC_R)       // refresh
 
 // right-side mod arrow cluster
-#define KC_RCLF RCMD_T(KC_LEFT) // right command under left arrow
-#define KC_RFRT LT(2, KC_RGHT)  // right fn (layer 2) under right arrow
 #define KC_RSUP RSFT_T(KC_UP)   // right shift under up arrow
-#define KC_RODN ROPT_T(KC_DOWN) // right option under down arrow
+#define KC_RFDN LT(2, KC_DOWN)  // right fn (layer 2) under down arrow
+#define KC_RCLF RCMD_T(KC_LEFT) // right command under left arrow
+#define KC_RORT ROPT_T(KC_RGHT) // right option under right arrow
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
@@ -63,8 +63,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
-        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSUP, KC_RFRT,
-                          KC_LOPT, KC_LCMD,                   LT(1,KC_SPC),                       KC_RCLF, KC_RODN
+        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSUP, KC_RFDN,
+                          KC_LOPT, KC_LCMD,                   LT(1,KC_SPC),                       KC_RCLF, KC_RORT
     ),
 
     [BASE_FN] = LAYOUT_60_hhkb(    // layer 1 BASE_FN - blue
