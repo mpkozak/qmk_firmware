6600c3f1e00c692336bef73fd344d236d55a2e49
mpkozak <mparkerkozak@gmail.com>
2024-06-13 23:03:58 -0400

    updates to m0116 keymap.

--- keyboards/converter/adb_usb/kozak/keymaps/m0116/keymap.c
+++ keyboards/converter/adb_usb/kozak/keymaps/m0116/keymap.c
@@ -76,19 +76,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [FN1] = LAYOUT_m0116(
                                                      QK_BOOT,
-        ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, KC_BRID, KC_BRIU, KC_VOLD, KC_VOLU, KC_MUTE,         QK_BOOT, ___x___, ___x___, KC_MUTE,
+        KC_LSCR, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, KC_BRID, KC_BRIU, KC_VOLD, KC_VOLU, KC_MUTE,         KC_LSCR, ___x___, ___x___, KC_MUTE,
         _______, ___x___, KC_UP,   ___x___, KC_REFR, KC_NTAB, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___,                  ___x___, KC_UP,   ___x___, KC_VOLU,
         _______, KC_LEFT, KC_DOWN, KC_RGHT, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___,          _______,         KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD,
         _______,          KC_DESK, KC_MVLS, KC_MVRS, KC_PSTT, ___x___, ___x___, KC_MCTL, KC_DESK, KC_MVLS, KC_MVRS,          _______,         ___x___, ___x___, ___x___,
         _______, _______, _______, _______,                   G(KC_SPC),                 _______, KC_MVLS, KC_MVRS, KC_DESK, _______,         _______,          ___x___, _______
     ),
     [BASE_SPD] = LAYOUT_m0116(
-                                                     TO(0),
-        TO(0),   KC_1,    KC_P2,   KC_P3,   KC_P4,   KC_P5,   KC_P6,   KC_P7,   KC_P8,   KC_9,    KC_0,    KC_MINS, ___x___, KC_BSPC,         KC_DEL,  KC_PEQL, KC_PSLS, KC_PAST,
+                                                     _______,
+        TO(0),   KC_1,    KC_P2,   KC_P3,   KC_P4,   KC_P5,   KC_P6,   KC_P7,   KC_P8,   KC_9,    KC_0,    KC_MINS, ___x___, KC_BSPC,         TO(0),   KC_PEQL, KC_PSLS, KC_PAST,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    ___x___, ___x___,                  KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
         ___x___, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,          KC_P4,   KC_P5,   KC_P6,   KC_PMNS,
         OSM(MOD_LSFT),    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_QUES,          KC_RSFT,         KC_P1,   KC_P2,   KC_P3,
-        ___x___, KC_LOPT, KC_LCMD, MO(2),                     LT(1,KC_SPC),              KC_CMDA, KC_OPTA, KC_RGHT, KC_DOWN, LT(2,KC_UP),     LT(2, KC_P0),     KC_PDOT, LT(2, KC_PENT)
+        ___x___, KC_LOPT, KC_LCMD, ___x___,                   KC_SPC,                    ___x___, KC_LEFT, KC_RGHT, KC_DOWN, KC_UP,           KC_P0,            KC_PDOT, KC_PENT
     )
 };
 
