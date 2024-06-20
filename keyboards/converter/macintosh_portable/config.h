88ea5d20dc3d69ce52d1e59080103efa4dfd5747
mpkozak <mparkerkozak@gmail.com>
2024-06-19 22:07:49 -0400

    macintosh_portable updates to keymap, matrix to pin mappings tested accurate. rp2040 board led now constant on, of when keydown.

--- keyboards/converter/macintosh_portable/config.h
+++ keyboards/converter/macintosh_portable/config.h
@@ -7,15 +7,16 @@
 
 #define MATRIX_HAS_GHOST
 
+#define LED_PIN GP17
+
 /* Double tap reset button to enter bootloader */
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U
 
 
+
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 // #define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
 // #define LOCKING_RESYNC_ENABLE
-
-// #define LED_PIN GP17
