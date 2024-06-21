b1d0fff258addc438e2f626938639db491244ca5
mpkozak <mparkerkozak@gmail.com>
2024-06-21 02:20:17 -0400

    fixes to portable keymap.

--- keyboards/converter/macintosh_portable/keymaps/kozak/config.c
+++ keyboards/converter/macintosh_portable/keymaps/kozak/config.c
@@ -4,3 +4,11 @@
 // Override Layer count
 #undef DYNAMIC_KEYMAP_LAYER_COUNT
 #define DYNAMIC_KEYMAP_LAYER_COUNT 3
+
+// Faster tap threshold
+#define TAPPING_TERM 160
+#undef PERMISSIVE_HOLD
+#undef PREVENT_STUCK_MODIFIERS
+
+// One-shot timeout
+#define ONESHOT_TIMEOUT 500
--- keyboards/converter/macintosh_portable/keymaps/kozak/keymap.c
+++ keyboards/converter/macintosh_portable/keymaps/kozak/keymap.c
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
+#include "keymap_user.h"
 
 #define KC_LSCR C(G(KC_Q))      // lock screen
 #define KC_EMOC C(G(KC_SPC))    // character picker
@@ -28,10 +29,6 @@
 #define KC_OPTA ROPT_T(KC_LEFT)         // right option + left arrow
 // #define KC_GRV2 LT(2,KC_GRV)            // backtick/grave + layer 2
 
-
-
-#define ___x___ KC_NO           // null
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Macintosh Portable
      * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬─────┐ ┌───┬───┬───┬───┐
@@ -47,21 +44,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───┴───┴──────┴───┴──────────────────┴───┴───┴───┴───┴───┘ └───────┴───┴───┘
      */
 
-    [0] = LAYOUT_5120_ansi(
+    [BASE] = LAYOUT_5120_ansi(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
         KC_LCAP, KC_LOPT, KC_LCMD, KC_CMDG,                   LT(1,KC_SPC),              KC_CMDE, KC_OPTA, KC_RGHT, KC_DOWN, LT(2,KC_UP)
     ),
-    [1] = LAYOUT_5120_ansi(
+    [FN0] = LAYOUT_5120_ansi(
         QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-        KC_INS,  KC_MVLT, KC_MVRT, KC_EMOC, KC_REFR, KC_NTAB, ___x___, ___x___, KC_UP,   ___x___, ___x___, KC_MVLS, KC_MVRS, KC_BSLS
+        KC_INS,  KC_MVLT, KC_MVRT, KC_EMOC, KC_REFR, KC_NTAB, ___x___, ___x___, KC_UP,   ___x___, ___x___, KC_MVLS, KC_MVRS, KC_BSLS,
         _______, KC_MVLS, KC_MVRS, KC_DESK, KC_FSTG, KC_LPAD, ___x___, KC_LEFT, KC_DOWN, KC_RGHT, ___x___, KC_MCTL,          _______,
         _______,          KC_ZMNS, KC_ZPLS, KC_ZACT, KC_PSTT, KC_WBAK, KC_WFOR, ___x___, KC_MVLT, KC_MVRT, KC_CMNT,          _______,
         _______, _______, _______, _______,                   _______,                   _______, _______, _______, _______, _______
     ),
-    [2] = LAYOUT_5120_ansi(
+    [FN1] = LAYOUT_5120_ansi(
         KC_LSCR, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, KC_BRID, KC_BRIU, KC_VOLD, KC_VOLU, KC_MUTE,
         _______, ___x___, KC_UP,   ___x___, KC_REFR, KC_NTAB, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___,
         _______, KC_LEFT, KC_DOWN, KC_RGHT, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___,          _______,
