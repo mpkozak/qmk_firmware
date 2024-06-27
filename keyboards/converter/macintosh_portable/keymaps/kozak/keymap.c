b6be537d2046d65e8b6b8d8a425f25aa54ac1794
mpkozak <mparkerkozak@gmail.com>
2024-06-26 20:15:21 -0400

    removed left+right arrows from macintosh_portable speed autocorrect layer.

--- keyboards/converter/macintosh_portable/keymaps/kozak/keymap.c
+++ keyboards/converter/macintosh_portable/keymaps/kozak/keymap.c
@@ -101,7 +101,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    ___x___, ___x___, KC_BSPC,
         ___x___, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
         OSM(MOD_LSFT),    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_QUES,          KC_RSFT,
-        _______, KC_LOPT, KC_LCMD, ___x___,                   KC_SPC,                    ___x___, KC_LEFT, KC_RGHT, KC_DOWN, KC_UP
+        _______, KC_LOPT, KC_LCMD, ___x___,                   KC_SPC,                    ___x___, ___x___, ___x___, KC_DOWN, KC_UP
     )
 };
 
