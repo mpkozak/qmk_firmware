b0058cb3ad3375077465e5dae22050a2108791d7
mpkozak <mparkerkozak@gmail.com>
2024-07-09 22:31:42 -0400

    working on m0115 adb implementation.

--- _kozak/init.sh
+++ _kozak/init.sh
@@ -20,8 +20,8 @@ _q0="$QMK_HOME/keyboards/keychron/q0"
 _q2="$QMK_HOME/keyboards/keychron/q2"
 _q60="$QMK_HOME/keyboards/keychron/q60"
 _tempo="$QMK_HOME/keyboards/mode/m60h"
-_adb="$QMK_HOME/keyboards/converter/adb_usb"
 _m0110="$QMK_HOME/keyboards/converter/m0110_usb"
+_adb="$QMK_HOME/keyboards/converter/adb_usb"
 _portable="$QMK_HOME/keyboards/converter/macintosh_portable"
 
 # custom keymaps
@@ -29,10 +29,11 @@ _q0km="$_q0/plus/keymaps/kozak"
 _q2km="$_q2/ansi_encoder/keymaps/kozak"
 _q60km="$_q60/ansi/keymaps/kozak"
 _tempokm="$_tempo/keymaps/kozak"
-_m0116km="$_adb/kozak/keymaps/m0116"
 _m0110km="$_m0110/kozak/keymaps/m0110"
 _m0120km="$_m0110/kozak/keymaps/m0110_m0120"
 _m0110akm="$_m0110/kozak/keymaps/m0110a"
+_m0115km="$_adb/kozak/keymaps/m0115"
+_m0116km="$_adb/kozak/keymaps/m0116"
 _portablekm="$_portable/keymaps/kozak"
 
 # customized keyboards
@@ -40,8 +41,8 @@ _q0c="keychron/q0/plus"
 _q2c="keychron/q2/ansi_encoder"
 _q60c="keychron/q60/ansi"
 _tempoc="mode/m60h"
-_adbc="converter/adb_usb/kozak"
 _m0110c="converter/m0110_usb/kozak"
+_adbc="converter/adb_usb/kozak"
 _portablec="converter/macintosh_portable"
 
 # build filenames
@@ -49,10 +50,11 @@ _q0f="keychron_q0_plus_kozak.bin"
 _q2f="keychron_q2_ansi_encoder_kozak.bin"
 _q60f="keychron_q60_ansi_kozak.bin"
 _tempof="mode_m60h_kozak.bin"
-_m0116f="converter_adb_usb_kozak_m0116.hex"
 _m0110f="converter_m0110_usb_kozak_m0110.hex"
 _m0120f="converter_m0110_usb_kozak_m0110_m0120.hex"
 _m0110af="converter_m0110_usb_kozak_m0110a.hex"
+_m0115f="converter_adb_usb_kozak_m0115.hex"
+_m0116f="converter_adb_usb_kozak_m0116.hex"
 _portablef="converter_macintosh_portable_kozak.uf2"
 
 
@@ -65,17 +67,18 @@ alias q0='cd "$_q0"; e'
 alias q2='cd "$_q2"; e'
 alias q60='cd "$_q60"; e'
 alias tempo='cd "$_tempo"; e'
-alias adb='cd "$_adb"; e'
 alias m0110='cd "$_m0110"; e'
+alias adb='cd "$_adb"; e'
 alias portable='cd $_portable; e'
 alias ek='e "$KOZAK"'
 alias e0='cd $_q0km; e'
 alias e2='cd $_q2km; e'
 alias e60='cd $_q60km; e'
 alias etempo='cd $_tempokm; e'
-alias em0116='cd $_m0116km; e'
 alias em0110='cd "$_m0110km"; e'
 alias em0110a='cd "$_m0110akm"; e'
+alias em0115='cd $_m0115km; e'
+alias em0116='cd $_m0116km; e'
 
 # autocorrect aliases
 alias ace='e "$KOZAK"/autocorrect_dictionary.txt'
@@ -83,6 +86,7 @@ alias ace='e "$KOZAK"/autocorrect_dictionary.txt'
 alias acrm_q2='[ -e "$_q2"/ansi_encoder/keymaps/kozak/autocorrect_data.h ] && rm "$_q2"/ansi_encoder/keymaps/kozak/autocorrect_data.h'
 alias acrm_q60='[ -e "$_q60"/ansi/keymaps/kozak/autocorrect_data.h ] && rm "$_q60"/ansi/keymaps/kozak/autocorrect_data.h'
 # alias acrm_tempo='[ -e "$_tempo"/keymaps/kozak/autocorrect_data.h ] && rm "$_tempo"/keymaps/kozak/autocorrect_data.h'
+alias acrm_m0115='[ -e "$_adb"/kozak/keymaps/m0115/autocorrect_data.h ] && rm "$_adb"/kozak/keymaps/m0115/autocorrect_data.h'
 alias acrm_m0116='[ -e "$_adb"/kozak/keymaps/m0116/autocorrect_data.h ] && rm "$_adb"/kozak/keymaps/m0116/autocorrect_data.h'
 alias acrm_portable='[ -e "$_portable"/keymaps/kozak/autocorrect_data.h ] && rm "$_portable"/keymaps/kozak/autocorrect_data.h'
 alias acrm="acrm_q2 && acrm_q60 && acrm_m0116 && acrm_portable"
@@ -90,14 +94,16 @@ alias acrm="acrm_q2 && acrm_q60 && acrm_m0116 && acrm_portable"
 alias acg_q2='qmk generate-autocorrect-data "$KOZAK"/autocorrect_dictionary.txt -kb keychron/q2/ansi_encoder -km kozak'
 alias acg_q60='qmk generate-autocorrect-data "$KOZAK"/autocorrect_dictionary.txt -kb keychron/q60/ansi -km kozak'
 # alias acg_tempo='qmk generate-autocorrect-data "$KOZAK"/autocorrect_dictionary.txt -kb mode/m60h -km kozak'
+alias acg_m0115='qmk generate-autocorrect-data "$KOZAK"/autocorrect_dictionary.txt -kb converter/adb_usb/kozak -km m0115'
 alias acg_m0116='qmk generate-autocorrect-data "$KOZAK"/autocorrect_dictionary.txt -kb converter/adb_usb/kozak -km m0116'
 alias acg_portable='qmk generate-autocorrect-data "$KOZAK"/autocorrect_dictionary.txt -kb converter/macintosh_portable -km kozak'
-alias acg="acg_q2 && acg_q60 && acg_m0116 && acg_portable"
+alias acg="acg_q2 && acg_q60 && acg_m0115 && acg_m0116 && acg_portable"
 
 alias acr="acrm; acg"
 alias acr_q2='acrm_q2; acg_q2'
 alias acr_q60='acrm_q60; acg_q60'
 alias acr_tempo='acrm_tempo; acg_tempo'
+alias acr_m0115='acrm_m0115; acg_m0115'
 alias acr_m0116='acrm_m0116; acg_m0116'
 alias acr_portable='acrm_portable; acg_portable'
 
@@ -131,18 +137,11 @@ function build {
         board_name="tempo"
         kb="$_tempoc"
         file="$_tempof"
-    elif [[ "$1" == "m0116" ]]; then
-        board_name="m0116"
-        kb="$_adbc"
-        km="m0116"
-        file="$_m0116f"
-        ext="hex"
-        ac=1
-    elif [[ "$1" == "m0110a" ]]; then
-        board_name="m0110a"
+    elif [[ "$1" == "m0110" ]]; then
+        board_name="m0110"
         kb="$_m0110c"
-        km="m0110a"
-        file="$_m0110af"
+        km="m0110"
+        file="$_m0110f"
         ext="hex"
     elif [[ "$1" == "m0120" ]]; then
         board_name="m0110-m0120"
@@ -150,12 +149,26 @@ function build {
         km="m0110_m0120"
         file="$_m0120f"
         ext="hex"
-    elif [[ "$1" == "m0110" ]]; then
-        board_name="m0110"
+    elif [[ "$1" == "m0110a" ]]; then
+        board_name="m0110a"
         kb="$_m0110c"
-        km="m0110"
-        file="$_m0110f"
+        km="m0110a"
+        file="$_m0110af"
         ext="hex"
+    elif [[ "$1" == "m0115" ]]; then
+        board_name="m0115"
+        kb="$_adbc"
+        km="m0115"
+        file="$_m0115f"
+        ext="hex"
+        ac=1
+    elif [[ "$1" == "m0116" ]]; then
+        board_name="m0116"
+        kb="$_adbc"
+        km="m0116"
+        file="$_m0116f"
+        ext="hex"
+        ac=1
     elif [[ "$1" == "portable" ]]; then
         board_name="macintosh_portable"
         kb="$_portablec"
---
+++ keyboards/converter/adb_usb/kozak/keymaps/m0115/keymap.c
@@ -0,0 +1,166 @@
+#include QMK_KEYBOARD_H
+#include "keymap_user.h"
+
+#define KC_LSCR C(G(KC_Q))      // lock screen
+#define KC_EMOC C(G(KC_SPC))    // character picker
+#define KC_FSTG C(G(KC_F))      // fullscreen toggle
+#define KC_PSTT S(A(G(KC_V)))   // paste plaintext
+#define KC_ZMNS G(KC_MINS)      // zoom out
+#define KC_ZPLS G(KC_EQL)       // zoom in
+#define KC_ZACT G(KC_0)         // zoom actual
+#define KC_MVLT LSG(KC_LBRC)    // move left tab
+#define KC_MVRT LSG(KC_RBRC)    // move right tab
+#define KC_MVLS C(KC_LEFT)      // move left space
+#define KC_MVRS C(KC_RGHT)      // move right space
+#define KC_DESK KC_F11          // show desktop
+#define KC_WBAK G(KC_LBRC)      // browser back
+#define KC_WFOR G(KC_RBRC)      // browser forward
+#define KC_CMNT G(KC_SLSH)      // comment shortcut
+#define KC_NTAB G(KC_T)         // new tab
+#define KC_REFR LSG(KC_R)       // refresh
+#define ___x___ KC_NO           // null
+
+// M0116-specific
+// #define KC_CMDG LCMD_T(KC_GRV)          // left command + grave
+// #define KC_CMDA RCMD_T(KC_BSLS)         // right command + backslash
+// #define KC_OPTA ROPT_T(KC_LEFT)         // right option + left arrow
+// #define KC_GRV2 LT(2,KC_GRV)            // backtick/grave + layer 2
+
+
+const key_override_t comma_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_COMM, KC_COMM, 1 << BASE_SPD);
+const key_override_t period_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_DOT, KC_DOT, 1 << BASE_SPD);
+const key_override_t hyphen_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_MINS, KC_MINS, 1 << BASE_SPD);
+
+// This globally defines all key overrides to be used
+const key_override_t **key_overrides = (const key_override_t *[]){
+    &comma_override,
+    &period_override,
+    &hyphen_override,
+    NULL // Null terminate the array of overrides!
+};
+
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* M0115
+     * ┌───┐   ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┐             ┌───┐
+     * |Esc|   |F1 |F2 |F3 |F4 | |F5 |F6 |F7 |F8 | |F9 |F10|F11|F12| |F13|F14|F15|             | ◀︎ |
+     * └───┘   └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┘             └───┘
+     *
+     *
+     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐ ┌───┬───┬───┐ ┌───┬───┬───┬───┐
+     * |Esc|1  |2  |3  |4  |5  |6  |7  |8  |9  |0  |-  |=  |Delete | |Ins|Hom|PUp| |Clr|=  |/  |*  |
+     * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤ ├───┼───┼───┤ ├───┼───┼───┼───┤
+     * |Tab  |Q  |W  |E  |R  |T  |Y  |U  |I  |O  |P  |[  |]  |\    | |Del|End|PDn  |8  |9  |+  |-  |
+     * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤ └───┴───┴───┘ ├───┼───┼───┼───┤
+     * |CapsLk|A  |S  |D  |F  |G  |H  |J  |K  |L  |;  |'  |Return  |               |4  |5  |6  |+  |
+     * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────────┤     ┌───┐     ├───┼───┼───┼───┤
+     * |Shift   |Z  |X  |C  |V  |B  |N  |M  |,  |.  |/  |Shift     |     |↑  |     |1  |2  |3  |   |
+     * ├─────┬──┴┬──┴───┼───┴───┴───┴───┴───┴───┴─┬─┴───┼───┬──────┤ ┌───┼───┼───┐ ├───┴───┼───┤Ent|
+     * |Ctrl |Opt|⌘     |                         |⌘    |Opt|Ctrl  | |←  |↓  |→  | |0      |.  |   |
+     * └─────┴───┴──────┴─────────────────────────┴─────┴───┴──────┘ └───┴───┴───┘ └───────┴───┴───┘
+     */
+    [BASE] = LAYOUT_m0115(
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                   KC_PSCR, KC_SCRL, KC_PAUS,                                    KC_EJCT,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,         KC_INS,  KC_HOME, KC_PGUP,         KC_NUM,  KC_PEQL, KC_PSLS, KC_PAST,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,         KC_DEL,  KC_END,  KC_PGDN,         KC_P7,   KC_P8,   KC_P9,   KC_PMNS,
+        KC_LCAP, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,                                             KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
+        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,                  KC_UP,                    KC_P1,   KC_P2,   KC_P3,
+        KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                                      ___x___, KC_RALT, KC_RCTL,         KC_LEFT, KC_DOWN, KC_RGHT,         KC_P0,            KC_PDOT, KC_PENT
+    ),
+    [FN0] = LAYOUT_m0115(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                  _______, _______, _______,                                    _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,         _______, _______, _______,         _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,         _______, _______, _______,         _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,                                            _______, _______, _______, _______,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,                  _______,                  _______, _______, _______,
+        _______, _______, _______,                            _______,                                     _______, _______, _______,         _______, _______, _______,         _______,          _______, _______
+    ),
+    [FN1] = LAYOUT_m0115(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                  _______, _______, _______,                                    _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,         _______, _______, _______,         _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,         _______, _______, _______,         _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,                                            _______, _______, _______, _______,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,                  _______,                  _______, _______, _______,
+        _______, _______, _______,                            _______,                                     _______, _______, _______,         _______, _______, _______,         _______,          _______, _______
+    ),
+    [BASE_SPD] = LAYOUT_m0115(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                  _______, _______, _______,                                    _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,         _______, _______, _______,         _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,         _______, _______, _______,         _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,                                            _______, _______, _______, _______,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,                  _______,                  _______, _______, _______,
+        _______, _______, _______,                            _______,                                     _______, _______, _______,         _______, _______, _______,         _______,          _______, _______
+    ),
+};
+
+
+
+void keyboard_post_init_user(void) {
+#ifdef AUTOCORRECT_OFF_AT_STARTUP
+    // toggle autocorrect off at startup
+    if (autocorrect_is_enabled()) {
+        autocorrect_toggle();
+    }
+#endif
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    switch (get_highest_layer(state)) {
+        case BASE_SPD:
+            if (!autocorrect_is_enabled()) {
+                autocorrect_enable();
+            }
+            break;
+        default:
+            if (autocorrect_is_enabled()) {
+                autocorrect_disable();
+            }
+            break;
+    }
+    return state;
+}
+
+bool is_number(uint16_t keycode) {
+    switch (keycode) {
+        case KC_P1 ... KC_P0:
+        case KC_1 ... KC_0:
+            return true;
+        default:
+            return false;
+    }
+}
+
+bool is_alpha (uint16_t keycode) {
+    switch (keycode) {
+        case KC_A ... KC_Z:
+            return true;
+        default:
+            return false;
+    }
+}
+
+bool is_prev_alpha = false;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    // ignore suprious number keys in the middle of alphas for speed layer
+    if (get_highest_layer(layer_state) == BASE_SPD) {
+        if (is_number(keycode)) {
+            if (is_prev_alpha) {
+                return false;
+            }
+        }
+        // update previous keycode is_alpha state
+        if (record->event.pressed) {
+            if (is_alpha(keycode)) {    // keycode is alpha
+                if (!is_prev_alpha) {
+                    is_prev_alpha = true;
+                }
+            } else {                    // keycode is not alpha
+                if (is_prev_alpha) {
+                    is_prev_alpha = false;
+                }
+            }
+        }
+    }
+    return true;
+}
