44932219b2df122393200127375a9b47197a0992
mpkozak <mparkerkozak@gmail.com>
2024-07-10 00:26:52 -0400

    updates to m0115.

--- keyboards/converter/adb_usb/kozak/rules.mk
+++ keyboards/converter/adb_usb/kozak/rules.mk
@@ -3,9 +3,9 @@
 #
 BOOTMAGIC_ENABLE = yes    # Enable Bootmagic Lite
 MOUSEKEY_ENABLE  = no     # Mouse keys
-CONSOLE_ENABLE   = no     # Console for debug
-COMMAND_ENABLE   = no     # Commands for debug and configuration
-NKRO_ENABLE      = no     # Enable N-Key Rollover
+CONSOLE_ENABLE   = yes    # Console for debug
+COMMAND_ENABLE   = yes    # Commands for debug and configuration
+NKRO_ENABLE      = yes    # Enable N-Key Rollover
 EXTRAKEY_ENABLE  = yes
 USB_HID_ENABLE   = yes
 BACKLIGHT_ENABLE = no
