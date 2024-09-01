54647543f981423592ca162a347219c46e5bafa8
mpkozak <mparkerkozak@gmail.com>
2024-09-01 17:48:55 -0400

    fix m0115 adb keymap design.

--- keyboards/converter/adb_usb/kozak/keymaps/m0115/keymap.c
+++ keyboards/converter/adb_usb/kozak/keymaps/m0115/keymap.c
@@ -74,21 +74,21 @@ const key_override_t **key_overrides = (const key_override_t *[]){
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* M0115
      * ┌───┐   ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┐             ┌───┐
-     * |Esc|   |F1 |F2 |F3 |F4 | |F5 |F6 |F7 |F8 | |F9 |F10|F11|F12| |F13|F14|F15|             | ◀︎ |
+     * |Esc|   |F1 |F2 |F3 |F4 | |F5 |F6 |F7 |F8 | |F9 |F10|F11|F12| |F13|F14|F15|             |◀︎  |
      * └───┘   └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┘             └───┘
      *
      *
      * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐ ┌───┬───┬───┐ ┌───┬───┬───┬───┐
-     * |Esc|1  |2  |3  |4  |5  |6  |7  |8  |9  |0  |-  |=  |Delete | |Ins|Hom|Pg↑| |Clr|=  |/  |*  |
+     * |`  |1  |2  |3  |4  |5  |6  |7  |8  |9  |0  |-  |=  |Delete | |Ins|Hom|Pg↑| |Clr|=  |/  |*  |
      * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤ ├───┼───┼───┤ ├───┼───┼───┼───┤
      * |Tab  |Q  |W  |E  |R  |T  |Y  |U  |I  |O  |P  |[  |]  |\    | |Del|End|Pg↓| |8  |9  |+  |-  |
      * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤ └───┴───┴───┘ ├───┼───┼───┼───┤
      * |CapsLk|A  |S  |D  |F  |G  |H  |J  |K  |L  |;  |'  |Return  |               |4  |5  |6  |+  |
      * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────────┤     ┌───┐     ├───┼───┼───┼───┤
      * |Shift   |Z  |X  |C  |V  |B  |N  |M  |,  |.  |/  |Shift     |     |↑  |     |1  |2  |3  |   |
-     * ├─────┬──┴┬──┴───┼───┴───┴───┴───┴───┴───┴─┬─┴───┼───┬──────┤ ┌───┼───┼───┐ ├───┴───┼───┤Ent|
-     * |Ctrl |Opt|⌘     |                         |⌘    |Opt|Ctrl  | |←  |↓  |→  | |0      |.  |   |
-     * └─────┴───┴──────┴─────────────────────────┴─────┴───┴──────┘ └───┴───┴───┘ └───────┴───┴───┘
+     * ├─────┬──┴─┬─┴───┼───┴───┴───┴───┴───┴───┴─┬─┴───┼────┬─────┤ ┌───┼───┼───┐ ├───┴───┼───┤Ent|
+     * |Ctrl |Opt |   ⌘ |                         | ⌘   |Opt |Ctrl | |←  |↓  |→  | |0      |.  |   |
+     * └─────┴────┴─────┴─────────────────────────┴─────┴────┴─────┘ └───┴───┴───┘ └───────┴───┴───┘
      */
 
     [BASE] = LAYOUT_m0115(
@@ -225,7 +225,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 
     // [BASE] = LAYOUT_m0115(
-    //     KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,          KC_PSCR, KC_SCRL, KC_PAUS,                                    QK_BOOT,
+    //     KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,          KC_PSCR, KC_SCRL, KC_PAUS,                                    KC_EJCT,
     //     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,         KC_INS,  KC_HOME, KC_PGUP,         KC_NUM,  KC_PEQL, KC_PSLS, KC_PAST,
     //     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,         KC_DEL,  KC_END,  KC_PGDN,         KC_P7,   KC_P8,   KC_P9,   KC_PMNS,
     //     KC_LCAP, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,                                             KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
