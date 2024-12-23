280322cc005129201d3015d47b8838eff61e1ee8
mpkozak <mparkerkozak@gmail.com>
2024-12-23 16:21:34 -0500

    refactoring of adb_usb mcu_led state handling.

--- keyboards/converter/adb_usb/kozak/mcu_leds.c
+++ keyboards/converter/adb_usb/kozak/mcu_leds.c
@@ -27,25 +27,56 @@ void mcu_led_enable(void) {
     gpio_set_pin_output(TX_LED);
 }
 
-void mcu_leds_on(void) {
-    gpio_write_pin_low(RX_LED);
-    gpio_write_pin_low(TX_LED);
-}
-
-void mcu_leds_off(void) {
+void mcu_led_toggle(uint8_t led_state) {
     gpio_write_pin_high(RX_LED);
     gpio_write_pin_high(TX_LED);
+    if (led_state & 0x01) {
+        gpio_write_pin_low(RX_LED);
+    }
+    if (led_state & 0x02) {
+        gpio_write_pin_low(TX_LED);
+    }
 }
 
 layer_state_t layer_state_set_mcu(layer_state_t state) {
     mcu_led_enable();
     switch (get_highest_layer(state)) {
+        case DFLT:
+            mcu_led_toggle(0x02);
+            break;
         case SPD:
-            mcu_leds_on();
+            mcu_led_toggle(0x03);
             break;
         default:
-            mcu_leds_off();
+            mcu_led_toggle(0x00);
             break;
     }
     return state;
 }
+
+
+
+
+
+// void mcu_leds_on(void) {
+//     gpio_write_pin_low(RX_LED);
+//     gpio_write_pin_low(TX_LED);
+// }
+
+// void mcu_leds_off(void) {
+//     gpio_write_pin_high(RX_LED);
+//     gpio_write_pin_high(TX_LED);
+// }
+
+// layer_state_t layer_state_set_mcu(layer_state_t state) {
+//     mcu_led_enable();
+//     switch (get_highest_layer(state)) {
+//         case SPD:
+//             mcu_leds_on();
+//             break;
+//         default:
+//             mcu_leds_off();
+//             break;
+//     }
+//     return state;
+// }
